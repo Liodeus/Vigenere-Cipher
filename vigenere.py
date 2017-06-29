873a8a633daf2366bec5fa8da6dc4b8c86db3bcf
Liodeus <thibaultgalbourdin@live.fr>
2017-06-29 20:55:40 +0200

    both encode and decode function are working

--- vigenere.py
+++ vigenere.py
@@ -3,15 +3,40 @@
 
 import Alphabet
 
-keyword = "DEFAULT"
-plaintext = "JOLIE"
 
-padding = (Alphabet.alpha[keyword[0]] - 1) + Alphabet.alpha[plaintext[0]]
+def encode(plaintext, keyword):
+    plaintextEncode = ""
+    i = 0
+    for nb in range(0, len(plaintext)):
+        if i < len(keyword):
+            padding = (Alphabet.alpha[keyword[i]] - 1) + \
+                Alphabet.alpha[plaintext[nb]]
+            plaintextEncode += Alphabet.beta[padding % 26]
+            i += 1
+
+        if i >= len(keyword):
+            i = 0
+
+    return plaintextEncode
 
 
-ciphertext = ""
-for nb in range(0, len(plaintext)):
-    padding = (Alphabet.alpha[keyword[nb]] - 1) + Alphabet.alpha[plaintext[nb]]
-    ciphertext += Alphabet.beta[padding]
+def decode(plaintext, keyword):
+    plaintextEncode = ""
+    i = 0
+    for nb in range(0, len(plaintext)):
+        if i < len(keyword):
+            padding = Alphabet.alpha[plaintext[nb]] - (Alphabet.alpha[keyword[i]] - 1)
+            if padding < 0:
+                padding += 26
+            
+            plaintextEncode += Alphabet.beta[padding % 26]
+            i += 1
 
-print(ciphertext)
+        if i >= len(keyword):
+            i = 0
+
+    return plaintextEncode
+
+
+print(encode("THIBAULT", "DEFAULT"))
+print(decode("WLNBUFEW", "DEFAULT"))
