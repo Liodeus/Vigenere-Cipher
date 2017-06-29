cd99ad2e4ef055332321f6b008a71c58ee675585
Liodeus <thibaultgalbourdin@live.fr>
2017-06-29 21:08:06 +0200

    Add a menu, uppercase for plaintext and keyword

--- vigenere.py
+++ vigenere.py
@@ -21,22 +21,38 @@ def encode(plaintext, keyword):
 
 
 def decode(plaintext, keyword):
-    plaintextEncode = ""
+    plaintextDecode = ""
     i = 0
     for nb in range(0, len(plaintext)):
         if i < len(keyword):
-            padding = Alphabet.alpha[plaintext[nb]] - (Alphabet.alpha[keyword[i]] - 1)
+            padding = Alphabet.alpha[plaintext[nb]] - \
+                (Alphabet.alpha[keyword[i]] - 1)
             if padding < 0:
                 padding += 26
-            
-            plaintextEncode += Alphabet.beta[padding % 26]
+
+            plaintextDecode += Alphabet.beta[padding % 26]
             i += 1
 
         if i >= len(keyword):
             i = 0
 
-    return plaintextEncode
+    return plaintextDecode
 
 
-print(encode("THIBAULT", "DEFAULT"))
-print(decode("WLNBUFEW", "DEFAULT"))
+if __name__ == "__main__":
+    print("[Vigenere, Thibault Galbourdin (github.com/Liodeus)]")
+    print("1- Encode")
+    print("2- Decode \n")
+
+    choice = input("Choice : ")
+
+    if choice == "1":
+        message = input("Plaintext : ")
+        keyword = input("Keyword :")
+        print(encode(message.upper(), keyword.upper()))
+    elif choice == "2":
+        message = input("Plaintext : ")
+        keyword = input("Keyword :")
+        print(decode(message.upper(), keyword.upper()))
+    else:
+        print("Error")
